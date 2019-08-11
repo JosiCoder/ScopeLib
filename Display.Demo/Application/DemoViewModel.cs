ff9701af3960b8066027fd04694c04cc4953c9ab
JosiCoder <JosiCoder@users.noreply.github.com>
2019-08-11 17:26:15 +0200

    Create sampler when building sequence providers

--- Display.Demo/Application/DemoViewModel.cs
+++ Display.Demo/Application/DemoViewModel.cs
@@ -158,22 +158,23 @@ namespace ScopeLib.Display.Demo
 
             // === Sample Sequences ===
 
-            var sampleSequenceProviders = BuildMainSampleSequenceProviders(sampleSequenceGenerators);
-            var sampler = new Sampler(sampleSequenceProviders, trigger, triggerChannelIndex);
-            scopeScreenVM.SampleSequenceProviders = sampler.SampleSequenceProviders;
+            var sampleSequenceProviders = BuildMainSampleSequenceProviders(sampleSequenceGenerators, trigger, triggerChannelIndex);
+            scopeScreenVM.SampleSequenceProviders = sampleSequenceProviders;
         }
 
         /// <summary>
         /// Builds a sequence provider for the main scope screen.
         /// </summary>
         private IEnumerable<Func<SampleSequence>> BuildMainSampleSequenceProviders(
-            IEnumerable<Func<SampleSequence>> sampleSequenceGenerators)
+            IEnumerable<Func<SampleSequence>> sampleSequenceGenerators,
+            ITrigger trigger, int triggerChannelIndex)
         {
             var sampleSequenceProviders = sampleSequenceGenerators.Select(ssg =>
             {
                 return new Func<SampleSequence>(() => ssg());
             });
-            return sampleSequenceProviders;
+            var sampler = new Sampler(sampleSequenceProviders, trigger, triggerChannelIndex);
+            return sampler.SampleSequenceProviders;
         }
 
         /// <summary>
